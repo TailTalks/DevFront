933460db781238122f77fe4f4aad50d66433a3f8
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-05-04 15:30:29 +0300

    add layout for wide dimensions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -33,7 +33,7 @@ function App() {
     <div className='w-full h-96'>
 
       {/* container */}
-      <div className='container mx-auto px-4 w-[1220px] flex-col'>
+      <div className='container mx-auto px-4 3xl:px-10 w-[1220px] flex-col 3xl:w-screen'>
 
         {/* header */}
         <div className='flex justify-between items-center h-14 mt-10 pt-2.5 pb-2.5'> 
@@ -41,23 +41,23 @@ function App() {
           {/* logo */}
           <div className='flex h-9 justify-between items-center'>
             <div className='w-9 h-9 pr-5 mr-5 bg-[#000000] dark:bg-white rounded-full bg'></div>
-            <div className='w-32 h-6 font-sans text-[#000000] dark:text-white text-2xl font-bold'>Tail Talks</div>
+            <div className='w-32 3xl:w-44 h-6 font-sans text-[#000000] dark:text-white 3xl:grow 3xl:text-4xl text-2xl font-bold'>Tail Talks</div>
           </div>
           {/* end logo */}
 
           {/* menu */}
-          <div className='flex justify-between items-center w-[725px] h-7'>
-            <button className='font-Rubik Variable text-[#000000] dark:text-white text-lg font-medium'>Наша миссия</button>
-            <button className='font-Rubik Variable text-[#000000] dark:text-white text-lg font-medium'>Принцип работы</button>
-            <button className='font-Rubik Variable text-[#000000] dark:text-white text-lg font-medium'>Дорожная карта</button>
-            <button className='font-Rubik Variable text-[#000000] dark:text-white text-lg font-medium'>Сообщество</button>
-            <button className='font-Rubik Variable text-[#000000] dark:text-white text-lg font-medium'>FAQ</button>
+          <div className='3xl:grow 3xl:px-96 3xl:text-3xl flex text-lg justify-between items-center w-[725px] h-7'>
+            <button className='font-Rubik Variable text-[#000000] dark:text-white font-medium'>Наша миссия</button>
+            <button className='font-Rubik Variable text-[#000000] dark:text-white font-medium'>Принцип работы</button>
+            <button className='font-Rubik Variable text-[#000000] dark:text-white font-medium'>Дорожная карта</button>
+            <button className='font-Rubik Variable text-[#000000] dark:text-white font-medium'>Сообщество</button>
+            <button className='font-Rubik Variable text-[#000000] dark:text-white font-medium'>FAQ</button>
           </div>
           {/* end menu */}
 
           {/* language and theme */}
             <div className='flex justify-between items-center ml-10'>
-              <select className='mr-5 bg-slate-300 font-Rubik Variable text-[#000000] dark:bg-black-50 dark:text-white  text-md font-medium rounded-md'>
+              <select className='mr-5 bg-slate-300 font-Rubik Variable text-[#000000] dark:bg-black-50 dark:text-white text-base 3xl:text-xl font-medium rounded-md'>
                 <option className='text-[#000000] dark:text-white'>RU</option>
                 <option className='text-[#000000] dark:text-white'>ENG</option>
                 <option className='text-[#000000] dark:text-white'>CHT</option>
@@ -74,12 +74,12 @@ function App() {
         {/* end header */}
 
         {/* main */}
-        <div className='flex justify-between items-center w-full h-[515px] mt-24 '> 
+        <div className='flex justify-between items-center w-full h-[515px] 3xl:h-[800px] mt-24 '> 
 
-          <div className='flex flex-col justify-start w-[526px] h-80'>
-            <div className='w-full h-32 font-sans text-[#000000] dark:text-white text-6xl font-semibold'>Социальная сеть Tail Talks</div>
-            <div className='w-[498px] h-14 mt-9 font-Rubik text-[#000000] dark:text-[#F3F3F3] text-xl font-light'>Место, где каждый питомец заслуживает быть звездой</div>
-            <button className='w-80 h-16 mt-9 rounded-2xl font-sans text-black text-lg font-bold bg-gradient-to-bl from-[#FFAE4E] to-[#FF4C8C]'>Узнать подробнее</button>
+          <div className='flex flex-col justify-start w-[526px] h-80 2xl:grow 3xl:grow'>
+            <div className='w-full h-32 font-sans text-[#000000] dark:text-white text-6xl font-semibold 3xl:text-7xl'>Социальная сеть Tail Talks</div>
+            <div className='w-[498px] 3xl:w-auto h-14 mt-9 font-Rubik text-[#000000] dark:text-[#F3F3F3] text-xl 3xl:text-3xl font-light'>Место, где каждый питомец заслуживает быть звездой</div>
+            <button className='w-80 h-16 3xl:w-96 3xl:h-24 mt-9 rounded-2xl font-sans text-black text-lg 3xl:text-2xl font-bold bg-gradient-to-bl from-[#FFAE4E] to-[#FF4C8C]'>Узнать подробнее</button>
           </div>
 {/* 
           <div className='relative w-[600px] h-[515px]'>
@@ -89,21 +89,23 @@ function App() {
             <img className='absolute top-52 right-5 cursor-pointer' src={Maps} alt='Maps'></img>
           </div> */}
 
-          <img className='relative w-[600px] h-[515px]' src={theme ? illustrationDark : illustrationLight} alt='illustration'></img>
+          <img className='relative 3xl:w-[900px] 3xl:h-[715px] w-[600px] h-[515px] 2xl:grow' src={theme ? illustrationDark : illustrationLight} alt='illustration'></img>
 
         </div>
         {/* end main */}
 
         {/* mission */}
-        <div className='flex justify-between w-full h-[355px] mt-36'>
-          <div className='w-[542px]'>
-            <div className='w-[380px] h-16 font-sans text-[#000000] dark:text-white text-5xl font-medium'>Наша миссия</div>
-            <div className='w-full mt-7 font-Rubik text-[#000000] dark:text-[#F3F3F3] text-xl font-light'>Это не просто социальная сеть, это место, где каждый питомец становится звездой, а каждый владелец находит друга.</div>
-            <div className='w-full mt-7 font-Rubik text-[#000000] dark:text-[#F3F3F3] text-xl font-light'>Мы стремимся к созданию комьюнити, где общение между питомцами и их владельцами происходит естественно и радостно, 
+        <div className='flex justify-between w-full h-[355px] 3xl:h-[550px] mt-36'>
+          <div className='w-1/2 2xl:grow 3xl:pr-96'>
+            <div className='w-[380px] 3xl:w-auto h-16 font-sans text-[#000000] dark:text-white text-5xl 3xl:text-7xl font-medium'>Наша миссия</div>
+            <div className='w-full mt-7 3xl:mt-9 font-Rubik text-[#000000] dark:text-[#F3F3F3] text-xl 3xl:text-4xl font-light'>Это не просто социальная сеть, это место, где каждый питомец становится звездой, а каждый владелец находит друга.</div>
+            <div className='w-full mt-7 3xl:mt-9 font-Rubik text-[#000000] dark:text-[#F3F3F3] text-xl 3xl:text-4xl font-light'>Мы стремимся к созданию комьюнити, где общение между питомцами и их владельцами происходит естественно и радостно, 
               способствуя социальному развитию и благополучию любителей домашних животных.
             </div>
           </div>
-          <img className='w-[590px] h-[350px] rounded-2xl' src={MissionCat} alt='MissionCat'></img>
+          <div className='w-1/2'>
+						<img className='w-[590px] h-[350px] 3xl:w-[900px] 3xl:h-[515px] rounded-2xl' src={MissionCat} alt='MissionCat' />
+					</div>
 
         </div>
         {/* end mission */}
@@ -115,8 +117,8 @@ function App() {
 							<div className='h-16 font-sans text-[#000000] dark:text-white text-5xl font-medium'>
 								Как работает Tail Talks?
 							</div>
-							<div className='mt-[30px] flex'>
-								<div className="w-96 min-w-96 h-[437px] bg-step-1 bg-center bg-cover flex flex-col justify-end">
+							<div className='mt-[30px] flex overflow-hidden'>
+								<div className="2xl:grow w-96 min-w-96 h-[437px] bg-step-1 bg-center bg-cover flex flex-col justify-end">
 									<div className='pl-10 pr-8 pb-12'>
 										<h3 className='font-sans font-bold text-white text-[26px]'>
 											Шаг 1
@@ -126,7 +128,7 @@ function App() {
 										</p>
 									</div>
 								</div>
-								<div className="w-96 min-w-96 h-[437px] bg-step-2 bg-center bg-cover flex flex-col justify-end">
+								<div className="2xl:grow w-96 min-w-96 h-[437px] bg-step-2 bg-center bg-cover flex flex-col justify-end">
 									<div className='pl-10 pr-8 pb-12'>
 										<h3 className='font-sans font-bold text-white text-[26px]'>
 											Шаг 2
@@ -136,7 +138,7 @@ function App() {
 										</p>
 									</div>
 								</div>
-								<div className="w-96 min-w-96 h-[437px] bg-step-3 bg-center bg-cover flex flex-col justify-end">
+								<div className="2xl:grow w-96 min-w-96 h-[437px] bg-step-3 bg-center bg-cover flex flex-col justify-end">
 									<div className='pl-10 pr-8 pb-12'>
 										<h3 className='font-sans font-bold text-white text-[26px]'>
 											Шаг 3
@@ -146,7 +148,7 @@ function App() {
 										</p>
 									</div>
 								</div>
-								<div className="w-96 min-w-96 h-[437px] bg-step-4 bg-center bg-cover flex flex-col justify-end">
+								<div className="2xl:grow w-96 min-w-96 h-[437px] bg-step-4 bg-center bg-cover flex flex-col justify-end">
 									<div className='pl-10 pr-8 pb-12'>
 										<h3 className='font-sans font-bold text-white text-[26px]'>
 											Шаг 4
@@ -156,7 +158,7 @@ function App() {
 										</p>
 									</div>
 								</div>
-								<div className="w-96 min-w-96 h-[437px] bg-step-5 bg-center bg-cover flex flex-col justify-end">
+								<div className="2xl:grow w-96 min-w-96 h-[437px] bg-step-5 bg-center bg-cover flex flex-col justify-end">
 									<div className='pl-10 pr-8 pb-12'>
 										<h3 className='font-sans font-bold text-white text-[26px]'>
 											Шаг 5
@@ -173,35 +175,35 @@ function App() {
 
 				{/*TailTalks in telegram */}
 
-				<div className='mt-[140px] flex justify-between h-[666px] text-center gap-x-12'>
-					<div className='flex flex-col justify-between w-1/2 min-w-[590px] relative'>
-						<div className='pr-16'>
-							<h3 className='font-sans font-semibold dark:text-white text-[26px] text-black-50 mt-[72px]'>
+				<div className='mt-[140px] flex justify-between min-h-[666px] text-center gap-x-12'>
+					<div className='2xl:grow flex flex-col justify-between w-1/2 min-w-[590px] relative'>
+						<div className='pr-16 3xl:w-[790px]'>
+							<h3 className='font-sans font-semibold dark:text-white text-[26px] 3xl:text-4xl text-black-50 mt-[72px]'>
 								Станьте частью уникального сообщества любителей животных Tail Talks!
 							</h3>
-							<p className='dark:text-[#F3F3F3F2] font-light font-Rubik Variable text-[20px] leading-[26px] pt-4 text-[#4D4D4D]'>
+							<p className='dark:text-[#F3F3F3F2] font-light font-Rubik Variable text-[20px] 3xl:text-3xl leading-[26px] pt-4 3xl:mt-10 text-[#4D4D4D]'>
 								Не упусти шанс получить премиум-доступ к новой социальной сети для домашних животных.
 							</p>
 						</div>
-						<img src={telegramLabel} alt="" className='absolute left-[-36px] bottom-[280px]'/>
-						<img src={telegramCat} alt="" className='rounded-2xl w-[590px] h-[350px]'/>
+						<img src={telegramLabel} alt="" className='absolute left-[-36px] bottom-[280px] 3xl:bottom-[430px]'/>
+						<img src={telegramCat} alt="" className='rounded-2xl w-[590px] h-[350px] 3xl:w-[790px] 3xl:h-[500px] 3xl:mt-10'/>
 					</div>
-					<div className='flex flex-col justify-between w-1/2 min-w-[590px] relative'>
-						<div>
-							<img src={telegramDog} alt="" className='rounded-2xl w-[590px] h-[350px]'/>
-							<img src={notesLabel} alt="" className='absolute right-[-36px] top-[-36px]'/>
-							<h3 className='font-sans font-semibold dark:text-white text-[26px] text-black-50 mt-20'>
-								Сообщество Tail Talks в Telegram
-							</h3>
-							<p className='w-[560px] dark:text-[#F3F3F3F2] font-light font-Rubik Variable text-[20px] leading-[26px] pt-4 text-[#4D4D4D]'>
-							Подключайтесь к нашему Telegram-сообществу, чтобы вместе создавать лучшее место для общения любителей животных. Ваши идеи помогут нам расти и развиваться!
-							</p>
-							<a href="https://t.me/tailtalkstg" target='_blank'>
-								<button className='font-sans font-bold text-lg w-96 h-16 rounded-2xl bg-orangeGradient mt-10'>
-									Сообщество Telegram
-								</button>
-							</a>
-						</div>
+					<div className='2xl:grow flex flex-col justify-between items-center w-1/2 min-w-[590px] relative'>
+							<img src={telegramDog} alt="" className='rounded-2xl w-[590px] h-[350px] 3xl:w-[790px] 3xl:h-[500px] 3xl:mb-10'/>
+							<img src={notesLabel} alt="" className='absolute right-[-36px] 3xl:right-[180px] 4xl:right-[340px] top-[-36px]'/>
+							<div className='3xl:w-[790px]'>
+								<h3 className='font-sans font-semibold dark:text-white text-[26px] 3xl:text-4xl text-black-50 mt-20'>
+									Сообщество Tail Talks в Telegram
+								</h3>
+								<p className='w-[560px] 3xl:w-auto dark:text-[#F3F3F3F2] font-light font-Rubik Variable text-[20px] 3xl:text-3xl leading-[26px] pt-4 text-[#4D4D4D]'>
+								Подключайтесь к нашему Telegram-сообществу, чтобы вместе создавать лучшее место для общения любителей животных. Ваши идеи помогут нам расти и развиваться!
+								</p>
+								<a href="https://t.me/tailtalkstg" target='_blank'>
+									<button className='font-sans font-bold text-lg 3xl:text-2xl w-96 h-16 3xl:w-96 3xl:h-24 rounded-2xl bg-orangeGradient mt-10'>
+										Сообщество Telegram
+									</button>
+								</a>
+							</div>
 					</div>
 				</div>
 
@@ -209,7 +211,7 @@ function App() {
 
 				{/*registation*/}
 
-				<div className='h-[770px] bg-[#F3F3F3] rounded-3xl bg-counterDog bg-left bg-no-repeat mt-[140px] mx-[-62px] flex justify-end'>
+				<div className='h-[770px] bg-[#F3F3F3] rounded-3xl bg-counterDog bg-left bg-no-repeat mt-[140px] mx-[-62px] flex justify-end 3xl:justify-center'>
 					<div className='w-[805px] h-[500px] flex flex-col items-center mt-[134px] mb-[136px] pr-[110px]'>
 						<h3 className='font-sans text-[#000000] text-[34px] leading-[50px] font-medium'>
 						Отсчет времени до запуска нашей социальной сети 
