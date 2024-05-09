4da0a694384893f0855516f7b5ea2f37dd967924
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-05-09 21:32:20 +0300

    change layout on 1024px

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ module.exports = {
       'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+      'xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
 
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
